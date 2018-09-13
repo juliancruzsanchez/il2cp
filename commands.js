cd5b39d3f1f7eba613380ecc28bdc16f2a724bce
juliancruzsanchez <kickbutt.js@gmail.com>
2018-09-12 21:52:01 -0600

    Added comments

--- commands.js
+++ commands.js
@@ -1,15 +1,23 @@
-module.exports = function  (data, server) {
-    if (String(data).includes("is complete created")) {
+module.exports = function (data, server) {
+    let input = String(data);
+    // Welcomes users
+    if (input.includes("is complete created")) {
         var r = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
-        let ip = String(data).match(r)[0];
-        let un = String(data).split(",")[2].replace(/\s+/g, '');
-        console.log(ip,un)
+        let ip = input.match(r)[0];
+        let un = input.split(",")[2].replace(/\s+/g, '');
+        console.log(ip, un)
         server.exec(`chat Welcome ${un} to JagerOne TO ${un}`)
     }
-    else if (String(data).includes("<tl")) {
-        server.exec(`chat Time Remaining:`)
+    // Timeleft 
+    else if (input.includes("<tl")) {
+        server.exec(`chat Not Yet Implemented`)
     }
-    else if (String(data).includes("<?")) {
-        server.exec(`help`)
+    // Help 
+    else if (input.includes("<?")) {
+        server.exec(`chat Not Yet Implemented`)
+    } 
+    // Returns next map
+    else if (input.includes("<nextmap")) {
+        server.exec(`chat Not Yet Implemented`)
     }
 }
