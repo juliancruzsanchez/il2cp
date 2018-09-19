d728df76509d33da894afbc4e192a5a7a0015a10
Julian Sanchez <kickbutt.js@gmail.com>
2018-09-19 11:08:44 -0600

    Many Edit"

--- src/il2/lastManStanding.js
+++ src/il2/lastManStanding.js
@@ -1,22 +1,22 @@
 const il2 = require("./il2");
 module.exports = {
-  leaderBoard() {
-  
-      il2.chat(`+---+------+----------------+-------+`)
-      il2.chat(`| # | Team | Player         | Kills |`)
-      il2.chat(`+---+------+----------------+-------+`)
-      il2.chat(`| 1 |      | 88888888888888 |       |`)
-      il2.chat(`+---+------+----------------+-------+`)
-      il2.chat(`| 2 |      |                |       |`)
-      il2.chat(`+---+------+----------------+-------+`)
-      il2.chat(`| 3 |      |                |       |`)
-      il2.chat(`+---+------+----------------+-------+`)
-      il2.chat(`| 4 |      |                |       |`)
-      il2.chat(`+---+------+----------------+-------+`)
-      il2.chat(`| 5 |      |                |       |`)
-      il2.chat(`+---+------+----------------+-------+`)
+
+  leaderBoard(player) {
+    console.log(`+---+------+----------------+-------+`)
+    console.log(`| # | Team | Player         | Kills |`)
+    console.log(`+---+------+----------------+-------+`)
+    console.log(`| 1 |${player[0].team}|${player[0].name}|${player[0].kills}|`)
+    console.log(`+---+------+----------------+-------+`)
+    console.log(`| 2 |${player[1].team}|${player[1].name}|${player[1].kills}|`)
+    console.log(`+---+------+----------------+-------+`)
+    console.log(`| 3 |${player[2].team}|${player[2].name}|${player[2].kills}|`)
+    console.log(`+---+------+----------------+-------+`)
+    console.log(`| 4 |${player[3].team}|${player[3].name}|${player[3].kills}|`)
+    console.log(`+---+------+----------------+-------+`)
+    console.log(`| 5 |${player[4].team}|${player[4].name}|${player[4].kills}|`)
+    console.log(`+---+------+----------------+-------+`)
   },
-  planesLeftAlert(x){
-    il2.chat(`You have ${x} planes left!`)
+  planesLeftAlert(x) {
+    console.log(`You have ${x} planes left!`)
   }
 }
