b97de8777c53c6800e366084e1eca644217b6fcf
Julian Sanchez <kickbutt.js@gmail.com>
2018-09-18 18:44:39 -0600

    Fixed WIndows pAth 2

--- src/il2/configParser.js
+++ src/il2/configParser.js
@@ -1,5 +1,5 @@
 let fs = require("fs");
-let uf = process.env.APPDATA + "/" || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences/' : '/var/local');
+let uf = process.env.APPDATA + "\\" || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences/' : '/var/local');
 let loc = uf + "IL2 Control Panel/"
 module.exports = JSON.parse(fs.readFileSync(loc + "config.json")
   .toString())
