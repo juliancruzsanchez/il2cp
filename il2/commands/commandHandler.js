1e77c0db435f2d56a5d0854cebddc260ff4a57fa
Julian Sanchez <kickbutt.js@gmail.com>
2018-10-01 09:13:53 -0600

    Edits

--- il2/commands/commandHandler.js
+++ il2/commands/commandHandler.js
@@ -2,7 +2,7 @@ const UserCommand = require("./models/UserCommands").UserCommand
 const AdminCommand = require("./models/AdminCommands").AdminCommand
 
 module.exports = function commands(input, il2) {
-  
+
     function timeLeftAction() {
       console.log('a')
       let cm = require("../cycleManager")
@@ -13,14 +13,22 @@ module.exports = function commands(input, il2) {
       let tl = ts / 60000
       il2.chat(Math.ceil(tl) + " Minutes Remaining Until Next Mission", user)
     }
+    function help() {
+      il2.chat("Commands:", user)
+      il2.chat("<timeleft", user)
+      il2.chat("<nextmap", user)
+      il2.chat("<obj<blue", user)
+      il2.chat("<obj<red", user)
+      il2.chat("<help", user)
+      il2.chat("<timeleft", user)
+
+    }
     let user = input.match("Chat: (.*):")[1]
   
     new AdminCommand("nextmap", () => { require("../cycleManager").nextMission() }).exec(user,input)
     new UserCommand("obj<blue", () => {}).exec(user,input)
     new UserCommand("obj<red", () => {}).exec(user,input)
-    new UserCommand("help", () => { il2.chat("Commands:", user);il2.chat("<timeleft", user) }).exec(user, input)
+    new UserCommand("help", help).exec(user, input)
     new UserCommand("timeleft", timeLeftAction).exec(user,input)
-    new UserCommand("tl", timeLeftAction).exec(user,input)
-    new UserCommand("timeleft", timeLeftAction).exec(user,input)
-    
+    new UserCommand("tl", timeLeftAction).exec(user,input)    
   }
