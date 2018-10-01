e68e4ccf50fc12c0c92eb18c2da30de86995c0a9
Julian Sanchez <kickbutt.js@gmail.com>
2018-09-30 22:53:20 -0600

    CLeaned Up comamnd code

--- il2/commands/commandHandler.js
+++ il2/commands/commandHandler.js
@@ -1,46 +1,25 @@
-let ac = require("./models/AdminCommands")
-let uc = require("./models/UserCommands")
+const UserCommand = require("./models/UserCommands").UserCommand
+const AdminCommand = require("./models/AdminCommands").AdminCommand
+
 module.exports = function commands(input, il2) {
-
-  function timeLeftAction() {
-    let cm = require("../cycleManager")
-    const config = require("../configParser")
-    let timeSince = new Date() - cm.getTime()
-    let tt = config.missionInterval * 60 * 1000
-    let ts = tt - timeSince
-    let tl = ts / 60000
-    il2.chat(Math.ceil(tl) + " Minutes Remaining Until Next Mission", user)
-  }
-  let user = input.match("Chat: (.*):")[1]
-
-  new ac.AdminCommand("nextmap", () => { require("../cycleManager").nextMission() }).exec(user, input)
-  let userCommands = [{
-      cmd: "help",
-      cc: function () {
-        il2.chat("Commands:", user)
-        il2.chat("<timeleft", user)
-      }
-    },
-    {
-      cmd: "obj<blue",
-      cc: function () {}
-    }, {
-      cmd: "obj<red",
-      cc: function () {}
-    }, {
-      cmd: "timeleft",
-      cc: function () {
-        timeLeftAction()
-      }
-    },
-    {
-      cmd: "tl",
-      cc: function () {
-        timeLeftAction()
-      }
+  
+    function timeLeftAction() {
+      let cm = require("../cycleManager")
+      const config = require("../configParser")
+      let timeSince = new Date() - cm.getTime()
+      let tt = config.missionInterval * 60 * 1000
+      let ts = tt - timeSince
+      let tl = ts / 60000
+      il2.chat(Math.ceil(tl) + " Minutes Remaining Until Next Mission", user)
     }
-  ]
-  for (let i in userCommands) {
-    new uc.UserCommand(i.cmd, i.cc)
+    let user = input.match("Chat: (.*):")[1]
+  
+    new AdminCommand("nextmap", () => { require("../cycleManager").nextMission() }).exec(user,input)
+    new UserCommand("obj<blue", () => {}).exec(user,input)
+    new UserCommand("obj<red", () => {}).exec(user,input)
+    new UserCommand("help", () => { il2.chat("Commands:", user);il2.chat("<timeleft", user) }).exec(user, input)
+    new UserCommand("timeleft", timeLeftAction).exec(user,input)
+    new UserCommand("tl", timeLeftAction).exec(user,input)
+    new UserCommand("timeleft", timeLeftAction).exec(user,input)
+    
   }
-}
