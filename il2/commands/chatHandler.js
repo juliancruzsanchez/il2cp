4bf84e65f89b9d534fc8461cb6147ff228150ee5
juliancruzsanchez <kickbutt.js@gmail.com>
2018-09-30 22:07:15 -0600

    Fixed User apge

--- il2/commands/chatHandler.js
+++ il2/commands/chatHandler.js
@@ -7,11 +7,20 @@ module.exports = function (data, server, il2) {
   let input = String(data)
   let isValidCommand = input.includes(": <") || input.includes(".");
   if (input.includes("is complete created")) {
-    newUser(input, il2)
+    newUser.add(input, il2)
   } else if (input.includes("Chat:") && !input.includes("Chat: ---") && !input.includes("Chat: Server") && isValidCommand) {
     commands(input, il2)
   } else if (input.includes("Chat: ---")) {
+    if (input.includes("has left the game")) {
+      console.log( newUser.users)
 
+      let un = input.split(" ")[2].replace(/\s+/g, '')
+      newUser.users.pop(newUser.users.some(function(user){
+        return user.username = un
+      }))
+    console.log( newUser.users)
+
+    }
   } else {
 
   }
--- il2/commands/newUsers.js
+++ il2/commands/newUsers.js
@@ -1,11 +1,14 @@
-module.exports= function(input, il2){  
+module.exports= {
+  users : [],
+  add(input, il2){  
     try {
       var r = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
       let ip = input.match(r)[0]
       let un = input.split(",")[2].replace(/\s+/g, '')
-      console.log(ip, un)
+      this.users.push( {ip: ip, username: un});
       il2.chat(`Welcome ${un} to JagerOne`, un)
     } catch (error) {
       console.error(error)
     }
   }
+}
