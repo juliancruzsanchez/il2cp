04011f894912e23295c963721b7fce296b28b496
Julian Sanchez <kickbutt.js@gmail.com>
2018-10-01 09:10:00 -0600

    edits

--- il2/commands/models/UserCommands.js
+++ il2/commands/models/UserCommands.js
@@ -1,7 +1,7 @@
 class UserCommand {
   constructor(command, action) {
-    this.command = command;
-    this.action = action;
+    this.command = command
+    this.action = action
     this.exec = (user, i) => {
       if (i.includes("<" + this.command)) {
         this.action()
