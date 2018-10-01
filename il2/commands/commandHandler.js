f7233757a10b5e2ffde320908e5096b028f97aa1
Julian Sanchez <kickbutt.js@gmail.com>
2018-09-30 22:47:53 -0600

    edits

--- il2/commands/commandHandler.js
+++ il2/commands/commandHandler.js
@@ -5,19 +5,43 @@ module.exports = function commands(input, il2) {
     function timeLeftAction() {
       let cm = require("../cycleManager")
       const config = require("../configParser")
-      let timeSince = new Date() - cm.getTime();
+      let timeSince = new Date() - cm.getTime()
       let tt = config.missionInterval * 60 * 1000
       let ts = tt - timeSince
       let tl = ts / 60000
       il2.chat(Math.ceil(tl) + " Minutes Remaining Until Next Mission", user)
     }
     let user = input.match("Chat: (.*):")[1]
-  
-    new ac.AdminCommand("nextmap", () => { require("../cycleManager").nextMission() }).exec(user,input)
-    new uc.UserCommand("obj<blue", () => {}).exec(user,input)
-    new uc.UserCommand("obj<red", () => {}).exec(user,input)
-    new uc.UserCommand("help", () => { il2.chat("Commands:", user) }).exec(user,input)
-    new uc.UserCommand("timeleft", timeLeftAction).exec(user,input)
-    new uc.UserCommand("tl", timeLeftAction).exec(user,input)
-    new uc.UserCommand("timeleft", timeLeftAction).exec(user,input)
-  }
+
+    new ac.AdminCommand("nextmap", () => { require("../cycleManager").nextMission() }).exec(user, input)
+    let userCommands = [{
+      cmd: "help",
+      cc: function () {
+        il2.chat("Commands:", user)
+        il2.chat("<timeleft", user)
+      }},
+      {
+        cmd: "obj<blue",
+        cc: function () {
+        }
+    }, {
+      cmd: "obj<red",
+      cc: function () {
+      }
+      }, {
+        cmd: "timeleft",
+        cc: function () {
+          timeLeftAction()
+        }
+      },
+      {
+        cmd: "tl",
+        cc: function () {
+          timeLeftAction()
+        }
+      }
+    ]
+    for (let i in userCommands){
+    new uc.UserCommand(i.cmd, i.cc)
+    }
+   }
