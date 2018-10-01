a2134b7573e90cb8a48a153c8483d9b2f0ef8eff
Julian Sanchez <kickbutt.js@gmail.com>
2018-09-30 22:48:11 -0600

    Cleaning

--- il2/commands/commandHandler.js
+++ il2/commands/commandHandler.js
@@ -2,46 +2,45 @@ let ac = require("./models/AdminCommands")
 let uc = require("./models/UserCommands")
 module.exports = function commands(input, il2) {
 
-    function timeLeftAction() {
-      let cm = require("../cycleManager")
-      const config = require("../configParser")
-      let timeSince = new Date() - cm.getTime()
-      let tt = config.missionInterval * 60 * 1000
-      let ts = tt - timeSince
-      let tl = ts / 60000
-      il2.chat(Math.ceil(tl) + " Minutes Remaining Until Next Mission", user)
-    }
-    let user = input.match("Chat: (.*):")[1]
+  function timeLeftAction() {
+    let cm = require("../cycleManager")
+    const config = require("../configParser")
+    let timeSince = new Date() - cm.getTime()
+    let tt = config.missionInterval * 60 * 1000
+    let ts = tt - timeSince
+    let tl = ts / 60000
+    il2.chat(Math.ceil(tl) + " Minutes Remaining Until Next Mission", user)
+  }
+  let user = input.match("Chat: (.*):")[1]
 
-    new ac.AdminCommand("nextmap", () => { require("../cycleManager").nextMission() }).exec(user, input)
-    let userCommands = [{
+  new ac.AdminCommand("nextmap", () => { require("../cycleManager").nextMission() }).exec(user, input)
+  let userCommands = [{
       cmd: "help",
       cc: function () {
         il2.chat("Commands:", user)
         il2.chat("<timeleft", user)
-      }},
-      {
-        cmd: "obj<blue",
-        cc: function () {
-        }
+      }
+    },
+    {
+      cmd: "obj<blue",
+      cc: function () {}
     }, {
       cmd: "obj<red",
+      cc: function () {}
+    }, {
+      cmd: "timeleft",
       cc: function () {
+        timeLeftAction()
       }
-      }, {
-        cmd: "timeleft",
-        cc: function () {
-          timeLeftAction()
-        }
-      },
-      {
-        cmd: "tl",
-        cc: function () {
-          timeLeftAction()
-        }
+    },
+    {
+      cmd: "tl",
+      cc: function () {
+        timeLeftAction()
       }
-    ]
-    for (let i in userCommands){
-    new uc.UserCommand(i.cmd, i.cc)
     }
-   }
+  ]
+  for (let i in userCommands) {
+    new uc.UserCommand(i.cmd, i.cc)
+  }
+}
